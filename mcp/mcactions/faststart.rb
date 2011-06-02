eb6e4573f9e68ef4d223b2ff9c94f2a4a9d488e9
Stephan Jorek <stephan.jorek@gmail.com>
2011-06-03 01:33:55 +0200

    renamed:    mcactions.rb -> mcp/mcactions.rb 	new file:   mcp/mcactions/faststart.rb 	renamed:    mcactions/reference.rb -> mcp/mcactions/reference.rb 	renamed:    mcqt/mcqt.rb -> mcp/mcqt/mcqt.rb

---
+++ mcp/mcactions/faststart.rb
@@ -0,0 +1,38 @@
+#
+#  Copyright (c) 2006-2009 Apple Inc.  All Rights Reserved.
+#
+#  IMPORTANT NOTE:  This file is licensed only for use on Apple-branded computers
+#  and is subject to the terms and conditions of the Apple Software License Agreement
+#  accompanying the package this file is a part of.  You may not port this file to
+#  another platform without Apple's written consent.
+#
+
+require 'actions/base'
+require 'mcqt/mcqt'
+
+module PodcastProducer
+  module Actions
+
+    class Faststart < Base
+      def usage
+        "faststart: optimize Quicktime or Mp4-alike for streaming\n\n" +
+        "usage: faststart --prb=PRB --input=INPUT\n" +
+        "                [--streamable] exits 0 or 1 e.g.:... && echo true || echo false\n"
+      end
+      def options
+        ["input*", "streamable"]
+      end
+      def run(arguments)
+        require_plural_option(:inputs, 1, 1)
+        
+        input = $subcommand_options[:inputs][0]
+        if $subcommand_options[:streamable]
+          is_streamable = McastQT.is_streamable?(input)
+          log_notice is_streamable.to_s
+          exit(is_streamable ? 0 : 1)
+        end
+      end
+    end
+    
+  end
+end
